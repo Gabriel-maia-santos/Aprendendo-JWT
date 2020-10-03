01cb6ba11d212e08e8b1ff1a1b0162665aa26c7a
Gabriel-maia-santos <gabis.maia.santos@gmail.com>
2020-10-03 03:31:16 -0300

    First commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using JWT.Contexts;
+using JWT.Domains;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        NyousContext _context = new NyousContext();
+
+        private IConfiguration _cfg;
+
+        public LoginController(IConfiguration cfg)
+        {
+            _cfg = cfg;
+        }
+
+        // Chamamos nosso método para validar nosso usuário da aplicação
+        private Usuario AuthenticateUser(Usuario login)
+        {
+            return _context.Usuario.Include(a => a.IdAcessoNavigation).FirstOrDefault(u => u.Email == login.Email && u.Senha == login.Senha);
+        }
+
+        // Criamos nosso método que vai gerar nosso Token
+        private string GenerateJSONWebToken(Usuario userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_cfg["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            //Definindo as claims
+            var claims = new[] {
+                new Claim(JwtRegisteredClaimNames.NameId, userInfo.Nome),
+                new Claim(JwtRegisteredClaimNames.Email, userInfo.Email),
+                new Claim(ClaimTypes.Role, userInfo.IdAcessoNavigation.Tipo),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            //Configurando o token
+            var token = new JwtSecurityToken
+                (
+                    _cfg["Jwt:Issuer"],
+                    _cfg["Jwt:Issuer"],
+                    claims,
+                    expires: DateTime.Now.AddMinutes(120),
+                    signingCredentials: credentials
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        // Usamos a anotação "AllowAnonymous" para 
+        // ignorar a autenticação neste método, já que é ele quem fará isso
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] Usuario login)
+        {
+            IActionResult response = Unauthorized();
+            var user = AuthenticateUser(login);
+            if (user != null)
+            {
+                var tokenString = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenString });
+            }
+            return response;
+        }
+
+    }
+}
